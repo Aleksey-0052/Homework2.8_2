8749f6857ac1533b27d3626a22c2ca982ac93195
alex <alexsemen666653@gmail.com>
2023-10-08 21:08:13 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/hw28/SkyproSpring13Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.hw28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpring13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpring13Application.class, args);
+	}
+
+}
